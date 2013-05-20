e90dfcfa345bc2cf91adda0e251a07542571afab
Aleš Roubíček <ales@roubicek.name>
2013-05-20 10:21:51 +0200

    Initial commit

---
+++ App_Start/WindsorActivator.cs.pp
@@ -0,0 +1,24 @@
+﻿using System;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof($rootnamespace$.App_Start.WindsorActivator), "PreStart")]
+[assembly: ApplicationShutdownMethodAttribute(typeof($rootnamespace$.App_Start.WindsorActivator), "Shutdown")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class WindsorActivator
+    {
+        static ContainerBootstrapper bootstrapper;
+
+        public static void PreStart()
+        {
+            bootstrapper = ContainerBootstrapper.Bootstrap();
+        }
+        
+        public static void Shutdown()
+        {
+            if (bootstrapper != null)
+                bootstrapper.Dispose();
+        }
+    }
+}
---
+++ Plumbing/WindsorControllerFactory.cs.pp
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+
+namespace $rootnamespace$.Plumbing
+{
+    public class WindsorControllerFactory : DefaultControllerFactory
+    {
+        readonly IWindsorContainer container;
+
+        public WindsorControllerFactory(IWindsorContainer container)
+        {
+            this.container = container;
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+                throw new HttpException(404, String.Format("The requested controller for path '{0}' could not be found", requestContext.HttpContext.Request.Path));
+            return (IController)container.Resolve(controllerType);
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            container.Release(controller);
+        }
+    }
+}
